04eb2a240c2d2223d5f64989319dd1e6c5fcbf81
thinhnd <nguyenducthinh0401@gmail.com>
2024-08-04 02:01:58 +0700

    add base table

---
+++ src/base/BaseTable.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+type TColumn = {
+  prop: string;
+  label: string;
+  width: string;
+};
+
+const props = defineProps({
+  data: {
+    type: Array,
+    required: true,
+  },
+  columns: {
+    type: Array<TColumn>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <el-table :data="props.data" style="width: 100%">
+    <el-table-column
+      v-for="column in columns"
+      :key="column.prop"
+      :prop="column.prop"
+      :label="column.label"
+      :width="column.width"
+    />
+    <el-table-column fixed="right" label="Thao tác" min-width="140">
+      <template #default>
+        <div class="icon-wrap">
+          <span class="icon">
+            <Plus style="width: 1em; height: 1em; color: green" />
+          </span>
+          <span class="icon">
+            <Edit style="width: 1em; height: 1em; color: blue" />
+          </span>
+          <span class="icon">
+            <Delete style="width: 1em; height: 1em; color: red" />
+          </span>
+        </div>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<style scoped lang="scss">
+.icon-wrap {
+  display: flex;
+  align-items: center;
+  .icon {
+    cursor: pointer;
+    width: 30px;
+    height: 30px;
+    border: 1px solid rgba(128, 128, 128, 0.336);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    margin-left: 8px;
+  }
+}
+</style>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,48 +1,53 @@
-<template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column type="selection" width="55" />
-    <el-table-column label="Date" width="120">
-      <template #default="scope">{{ scope.row.date }}</template>
-    </el-table-column>
-    <el-table-column property="name" label="Name" width="120" />
-    <el-table-column
-      property="address"
-      label="use show-overflow-tooltip"
-      width="240"
-      show-overflow-tooltip
-    />
-    <el-table-column property="address" label="address" />
-  </el-table>
-</template>
-
 <script lang="ts" setup>
-import { ElTable } from "element-plus";
+import BaseTable from "@/base/BaseTable.vue";
 
-interface User {
-  date: string;
-  name: string;
-  address: string;
-}
-const tableData: User[] = [
-  {
-    date: "2016-05-04",
-    name: "Aleyna Kutzner",
-    address: "Lohrbergstr. 86c, Süd Lilli, Saarland",
-  },
-  {
-    date: "2016-05-03",
-    name: "Helen Jacobi",
-    address: "760 A Street, South Frankfield, Illinois",
-  },
-  {
-    date: "2016-05-02",
-    name: "Brandon Deckert",
-    address: "Arnold-Ohletz-Str. 41a, Alt Malinascheid, Thüringen",
-  },
-  {
-    date: "2016-05-01",
-    name: "Margie Smith",
-    address: "23618 Windsor Drive, West Ricardoview, Idaho",
-  },
+const tableData = [
+  { date: "2023-01-01", name: "John Doe", address: "1234 Main St" },
+  { date: "2023-01-02", name: "Jane Doe", address: "5678 Main St" },
+];
+
+const tableColumns = [
+  { prop: "date", label: "Date", width: "180" },
+  { prop: "name", label: "Name", width: "180" },
+  { prop: "address", label: "Address", width: "180" },
+  { prop: "age", label: "age", width: "180" }
 ];
 </script>
+
+<template>
+  <div class="product-featured">
+    <el-card></el-card>
+    <el-card></el-card>
+    <el-card></el-card>
+  </div>
+  <div class="product-list">
+    <el-input style="width: 240px; height: 40px" placeholder="Tìm kiếm sản phẩm..." />
+    <BaseTable :data="tableData" :columns="tableColumns" />
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+  .product-featured{
+    width: 100%;
+    display: flex;
+    align-items: center;
+
+    .el-card{
+      height: 200px; 
+      flex: 1; 
+      margin: 20px;
+    }
+  }
+
+  .product-list{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 20px;
+    .el-input{
+      margin-bottom: 20px;
+    }
+  }
+</style>
